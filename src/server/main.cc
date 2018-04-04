170c2d54901546cea54e0189124f699a48589f52
Sid Vishnoi <sidvishnoi8@gmail.com>
2018-04-04 20:11:43 +0530

    Prepare merge with branch cpp

--- src/server/main.cc
+++ src/server/main.cc
@@ -4,20 +4,9 @@
  *  $ ./server [PORT=8000] [BACKLOG=5]
  */
 
-#include <iostream>
 #include "./server.h"
-#include "./db/db.h"
-
-using std::cout; using std::endl;
 
 int main(int argc, char *argv[]) {
-  User user("sid", "pass", "127.0.0.1", 7898, 4);
-  std::string u1 = user.serialize();
-  cout << u1  << endl;
-  User user2(u1);
-  cout << user2.serialize() << endl;
-
-  return 0;
   const int port = (argc > 1) ? std::atoi(argv[1]) : 8000;
   const int backlog = (argc > 2) ? std::atoi(argv[2]) : 5;
 
