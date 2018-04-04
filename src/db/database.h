614e67e85acd67be080dcb0481ab33dc853ed270
Sid Vishnoi <sidvishnoi8@gmail.com>
2018-04-04 20:08:44 +0530

    Add linear search based Database operations Remove binary search based operations (for now)

--- src/db/database.h
+++ src/db/database.h
@@ -6,43 +6,27 @@
 #include <cstdlib>
 #include <stdexcept>
 
-int getNumberOfLines(std::fstream &file, size_t lineLength);
-
 template <class T>
 class Database {
  public:
   explicit Database(
     const std::string &dir,
     const std::string &_pk,
-    std::string (*bkfn)(const T &) = nullptr)
-  : pk(_pk), dbDir(dir), bucketFn(bkfn) {
+    std::pair<int, int> _bucketHash = {0, 2}
+  ) : pk(_pk), dbDir(dir), bucketHash(_bucketHash) {
     if (dbDir.back() != '/') dbDir.append("/");
     createDbDir();
   }
 
-  // add an entry of class T (specializes Entry) to database
-  // entry is added in way such that db is sorted by primary key
+  // append an entry of class T (specializes Entry) to database
   int add(const T &);
 
-  // append an entry of class T
-  // !!! do not use with add() !!!
-  int append(const T &);
-
   // get an entry where primary key value is value
-  // uses binary search
-  T get(const std::string &value) const;
-
-  // get an entry from database where key = value
-  // uses Linear search
-  T get(const std::string &key, const std::string &value) const;
-
-  // TODO:
-  // get an entry based on a lambda comparison function
-  // uses Linear search
-
+  // uses linear search
+  std::pair<T, int> get(const std::string &value);
 
   // update an entry in database
-  bool update(T &oldUser, T &newUser);
+  bool update(T &oldEntry, T &newEntry);
 
   // mark an entry as "deleted"
   bool remove(T & entry);
@@ -51,26 +35,28 @@ class Database {
   // primary key
   std::string pk;
 
-  // create a new bucket based on name returned by bucketFn
-  std::string (*bucketFn)(const T &entry);
+  // bucket name is substring of pk at these indices
+  std::pair<int, int> bucketHash;
 
   // where to save database files
   std::string dbDir;
 
-  // get name of bucket for an entry using bucketFn
-  std::string getBucketName(const T &entry);
+  // get name of bucket for a pk value using bucketHash
+  std::string getBucketName(const std::string &pkValue) {
+    return dbDir + pkValue.substr(bucketHash.first, bucketHash.second) + ".txt";
+  }
 
   // create a bucket if not exists
   bool createBucket(const std::string &bucketName);
 
   // get number of records in a bucket
-  int getNumberOfRecords(const std::string &bucketName, const T &entry);
+  int getNumberOfRecords(std::fstream &bucket, const T &entry);
 
   // create the database directory
   void createDbDir();
 
   // helper function for add(const T&). uses binary search
-  int getInsertPosition(std::fstream &file, const T &entry);
+  int getInsertPosition(std::fstream &bucket, const T &entry);
 };
 
 template <class T>
@@ -84,24 +70,61 @@ void Database<T>::createDbDir() {
 }
 
 template <class T>
-std::string Database<T>::getBucketName(const T &entry) {
-  if (bucketFn != nullptr) return bucketFn(entry);
-  return entry.get(pk).substr(0, 2);
-}
-
-template <class T>
 bool Database<T>::createBucket(const std::string &bucketName) {
   std::ifstream fl(bucketName);
-  if (fl) return false;    // not created
+  if (fl) return false;   // not created
   const std::string cmd = "touch " + bucketName;
   return !system(cmd.c_str());
 }
 
 template <class T>
-int Database<T>::getNumberOfRecords(const std::string &bucketName, const T& entry) {
-  std::ifstream file(bucketName);
-  file.seekg(0, std::ios::end);
-  return file.tellg() / entry.size();
+int Database<T>::getNumberOfRecords(std::fstream &bucket, const T& entry) {
+  bucket.seekg(0, std::ios::end);
+  const int num = bucket.tellg() / entry.size();
+  bucket.seekg(0);
+  return num;
+}
+
+template <class T>
+int Database<T>::add(const T &entry) {
+  std::pair<T, int> old = get(entry.get(pk));
+  if (old.second != -1) return 1;
+  const std::string bucketName = getBucketName(entry.get(pk));
+  std::ofstream bucket(bucketName, std::ios::app | std::ios::binary);
+  bucket << entry.serialize() + "\n";
+  return 0;
+}
+
+template <class T>
+std::pair<T, int> Database<T>::get(const std::string &value) {
+  std::string record;
+  std::ifstream bucket(getBucketName(value), std::ios::binary);
+  while (std::getline(bucket, record) && !record.empty()) {
+    T temp(record);
+    if (temp.get(pk) == value) {
+      return {temp, bucket.tellg()};
+    }
+  }
+  return {T(), -1};
+}
+
+template <class T>
+bool Database<T>::update(T &oldEntry, T &newEntry) {
+  using namespace std;
+
+  pair<T, int> old = get(oldEntry.get(pk));
+
+  // do not update if some entry already exists with newEntry.pk
+  pair<T, int> same = get(newEntry.get(pk));
+  if (same.second != -1) return false;
+
+  ofstream bucket(
+    getBucketName(oldEntry.get(pk)),
+    ios::in | ios::out | ios::binary);
+  int offset = old.second - oldEntry.size() - 1;
+  bucket.seekp(offset);
+  bucket << newEntry.serialize() + "\n";
+  return true;
 }
 
 #endif  //  SRC_DB_DATABASE_H_
