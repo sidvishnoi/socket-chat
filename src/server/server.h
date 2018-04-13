45d3bfe3141bf212fd27b3cb6c803c1e4ebf8281
Sid Vishnoi <sidvishnoi8@gmail.com>
2018-04-13 20:22:51 +0530

    refactor server

--- src/server/server.h
+++ src/server/server.h
@@ -5,15 +5,18 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
+#include <sys/select.h>
+#include <sys/time.h>
 #include <iostream>
 #include <cstring>
 #include <cstdlib>
 #include <stdexcept>
 #include <cerrno>
-#include <sys/select.h>
-#include <sys/time.h>
+#include <map>
+#include <algorithm>
 
-using std::cout; using std::cin; using std::endl;
+using std::cout; using std::cin; using std::endl; using std::string;
+using FdToName = std::map<int, std::string>;
 
 void printError();
 
@@ -22,5 +25,8 @@ void Bind(int sockfd, int port);
 void Listen(int sockfd, int backlog);
 int Accept(int sockfd);
 int serverChat(int sockfd);
+
+const int BUFFER_SIZE = 1024;
+const string welcomeMsg("Welcome client");
 
 #endif  // SRC_SERVER_SERVER_H_
--- src/server/serverChat.cc
+++ src/server/serverChat.cc
@@ -1,69 +1,87 @@
 #include "./server.h"
 
+
 int serverChat(int sockfd) {
-  int buffer_size = 1024, client[5] = {0,0,0,0,0}, max_client = 5, sock, max = sockfd;
-  char buffer[buffer_size];
+  int max = sockfd;
+  char buffer[BUFFER_SIZE];
+  FdToName clients;
   fd_set master;
 
-  while(true) {
+  while (true) {
     FD_ZERO(&master);
     FD_SET(sockfd, &master);
-    for(int i = 0; i < max_client; i++) {
-      sock = client[i];
-      if(sock > 0)
-        FD_SET(sock, &master);
-      if(sock > max)
-        max = sock;
+    for (auto it = clients.begin(); it != clients.end(); ++it) {
+      FD_SET(it->first, &master);
+      if (it->first > max)
+        max = it->first;
     }
-    select(max+1, &master, NULL,NULL,NULL);
+    select(max+1, &master, NULL, NULL, NULL);
 
-    if(FD_ISSET(sockfd, &master)) {
-      int new_client = Accept(sockfd);
-      cout<<"Accepted new connection\n";
-      if(new_client > 0 && errno != EINTR) {
-        bool spaceFound = false;
-        // FD_SET(new_client, &master);
-        for(int i = 0; i < max_client; i++) {
-          if(client[i] <= 0) {
-            client[i] = new_client;
-            cout << new_client << "connected to server" << endl;
-            char msg[1024] = "Welcome client";
-            send(new_client, msg, sizeof(msg),0);  
-            spaceFound = true;
-            break;
-          }
-        }
-        if(!spaceFound) {
-          cout << "No space in connection list\n"; 
-          close(new_client);
-          continue;
-        }
+    if (FD_ISSET(sockfd, &master)) {
+      const int newClient = Accept(sockfd);
+      cout << newClient << " Accepted new connection" << endl;
+      if (newClient < 0) {
+        cout << "Cannot Accept from newClient" << endl;
+        continue;
       }
+      FD_SET(newClient, &master);
+      clients[newClient] = "";
+      send(newClient, welcomeMsg.c_str(), welcomeMsg.size(), 0);
+      continue;
     }
-    else {
-      for(int i = 0; i < max_client; i++) {
-        int current_client = client[i];
-        if(current_client > 0 && FD_ISSET(current_client, &master)) {
-          memset(buffer, 0 , buffer_size);
-          if(recv(current_client, buffer, sizeof(buffer), 0) <= 0) {
-            FD_CLR(current_client, &master);
-            close(current_client);
-            client[i] = -1;
-            cout << current_client << " is offline" << endl;
-            char msg[1024] = "client is offline";
-            for(int j = 0; j < max_client; j++) {
-              if (client[j] > 0 && client[j] != current_client)
-                send(client[j], msg, sizeof(msg), 0);
-            }
-          }
-          else {
-            for(int j = 0; j < max_client; j++) {
-              if(client[j] > 0 && client[j] != current_client) {
-                send(client[j], buffer, sizeof(buffer), 0);
+    for (auto it = clients.begin(); it != clients.end(); ++it) {
+      const int currentClientFd = it->first;
+      if (FD_ISSET(currentClientFd, &master)) {
+        memset(buffer, 0, BUFFER_SIZE);
+        // handle client connection lost
+        const int bytesRead = recv(currentClientFd, buffer, sizeof(buffer), 0);
+        if (bytesRead <= 0) {
+          FD_CLR(currentClientFd, &master);
+          close(currentClientFd);
+          string msg = clients[currentClientFd] + " is offline";
+          if (clients[currentClientFd] != "") {
+            for (auto itr = clients.begin(); itr != clients.end(); ++itr) {
+              if (itr->first != currentClientFd) {
+                // broadcast to all except the one lost
+                send(itr->first, msg.c_str(), msg.size(), 0);
               }
             }
           }
-          cout << buffer << endl;   
+        } else if (clients[currentClientFd].empty()) {
+          // broadcast new connection info
+          string newClientName(buffer);
+          string msg = string(newClientName) + " is online";
+          for (auto itr = clients.begin(); itr != clients.end(); ++itr) {
+            if (itr->first != currentClientFd) {
+              // broadcast to all except the new one
+              send(itr->first, msg.c_str(), msg.size(), 0);
+            } else {
+              clients[currentClientFd] = newClientName;
+            }
+          }
+        } else {
+          bool userFound = false;
+          string msg(buffer);
+          cout << msg << endl;
+          auto posAt = find(msg.begin(), msg.end(), '@');
+          if (posAt == msg.begin()) {
+            string clientName(++posAt, std::find(msg.begin(), msg.end(), ' '));
+            auto msgReceiver = find_if(clients.begin(),
+              clients.end(),
+              [&clientName](auto const &itr) -> bool {
+                return itr.second == clientName;
+            });
+            if (msgReceiver != clients.end()) userFound = true;
+            string temp = clients[currentClientFd] + " > " + msg;
+            send(msgReceiver->first, temp.c_str(), temp.size(), 0);
+          }
+          if (userFound) continue;
+          string temp = clients[currentClientFd] + " > " + msg;
+          for (auto itr = clients.begin(); itr != clients.end(); ++itr) {
+            if (itr->first != currentClientFd && !clients[itr->first].empty()) {
+              send(itr->first, temp.c_str(), temp.size(), 0);
+            }
+          }
         }
       }
     }
