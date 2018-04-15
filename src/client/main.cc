73fc0f027bee681316f7489b5f4dd5ff53f3f425
Sid Vishnoi <sidvishnoi8@gmail.com>
2018-04-16 00:06:38 +0530

    Move client to try-catch

--- src/client/main.cc
+++ src/client/main.cc
@@ -4,14 +4,18 @@ int main(int argc, char *argv[]) {
 	cout << ALT_SCREEN_ON;
 	const int port = (argc > 1) ? std::atoi(argv[1]) : 8000;
 	StrPair credentials = getCredentials();
-	cout << endl;
 
-	const int sockfd = Socket();
-	Connect(sockfd, port);
-
-	if (!login(sockfd, credentials)) return 1;
-	clientChat(sockfd, credentials.first);
-	close(sockfd);
+	try {
+		const int sockfd = Socket();
+		Connect(sockfd, port);
+		if (!login(sockfd, credentials)) return 1;
+		clientChat(sockfd, credentials.first);
+		close(sockfd);
+	} catch (std::exception &e) {
+		cout << ALT_SCREEN_OFF;
+		cout << color::red << "ERROR: " << e.what() << color::reset << endl;
+		return errno;
+	}
 	cout << ALT_SCREEN_OFF;
 	return 0;
 }
